55decaa8093a2905d3dfd4ee3b126d7dd6383b9b
Vasanth <vasanth@Vasanths-MacBook-Air.local>
2023-04-20 17:50:44 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,166 @@
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="mi">
+      <div className="iplteams">
+      <h1>IPL 2023 All Teams Squad, Captain, Players List.</h1>
+    </div>
+      <h1>Mumbai Indians squad for IPL 2023</h1>
+      <h2>Rohit Sharma. Captain</h2>
+      <h2>Suryakumar Yadav</h2>
+      <h2>Arjun Tendulkar</h2>
+      <h2>Jofra Archer</h2>
+      <h2>Tilak Varma</h2>
+      <h2>Tim David</h2>
+      <h2>Sandeep Warrier</h2>
+      <h2>Jhye Richardson</h2>
+      <h2>Hrithik Shokeen</h2>
+      <h2>Ramandeep Singh</h2>
+
+
+
+    <div className="srh">
+      <h1>Sunrisers Hyderabad squad for IPL 2023</h1>
+      <h2>Aiden Markram Captain</h2>
+      <h2>Abdul Samad</h2>
+      <h2>Rahul Tripathi</h2>
+      <h2>Glenn Phillips</h2>
+      <h2>Abhishek Sharma</h2>
+      <h2>Marco Jansen</h2>
+      <h2>Washington Sundar</h2>
+      <h2>Bhuvneshwar Kumar</h2>
+      <h2>Fazalhaq Farooqi</h2>
+      
+
+
+
+    <div className="pbks">
+      <h1>Punjab Kings squad for IPL 2023</h1>
+      <h2>Shikhar Dhawan. Captain</h2>
+      <h2>Jitesh Sharma</h2>
+      <h2>Prabhsimran Singh</h2>
+      <h2>Harpreet Brar</h2>
+      <h2>Raj Angad Bawa</h2>
+      <h2>Shahrukh Khan</h2>
+      <h2>Baltej Dhanda</h2>
+      <h2>Nathan Ellis</h2>
+      <h2>Sam Curran</h2>
+      <h2>Harpreet Bhatia</h2>
+      
+
+
+    <div className="lsg">
+      <h1>Lucknow Super Giants squad for IPL 2023</h1>
+      <h2>KL Rahul. Captain</h2>
+      <h2>Quinton de Kock</h2>
+      <h2>Deepak Hooda</h2>
+      <h2>Karan Sharma</h2>
+      <h2>Kyle Mayers</h2>
+      <h2>Avesh Khan</h2>
+      <h2>Mayank Yadav</h2>
+      <h2>Mark Wood</h2>
+      <h2>Mohsin Khan</h2>
+      <h2>Swapnil Singh</h2>
+      
+
+
+    <div className="gt">
+      <h1>Gujarat Titans squad for IPL 2023</h1>
+      <h2>Hardik Pandya Captain</h2>
+      <h2>David Miller</h2>
+      <h2>Matthew Wade</h2>
+      <h2>Jayant Yadav</h2>
+      <h2>Abhinav Manohar</h2>
+      <h2>Shubman Gill</h2>
+      <h2>Sai Kishore</h2>
+      <h2>Shivam Mavi</h2>
+      <h2>Rashid Khan</h2>
+      <h2>Odean Smith</h2>
+      
+      
+    <div className="rr">
+      <h1>Rajasthan Royals squad for IPL 2023</h1>
+      <h2>Sanju Samson. Captain</h2>
+      <h2>Jos Buttler</h2>
+      <h2>Yashasvi Jaiswal</h2>
+      <h2>Ravichandran Ashwin</h2>
+      <h2>KC Cariappa</h2>
+      <h2>uldip Yadav</h2>
+      <h2>Obed McCoy</h2>
+      <h2>Prasidh Krishna</h2>
+      <h2>Murugan Ashwin</h2>
+      <h2>Akash Vashisht</h2>
+    
+
+    <div className="dc">
+      <h1>Delhi Capitals squad for IPL 2023</h1>
+      <h2>David Warner. Captain</h2>
+      <h2>Rovman Powell</h2>
+      <h2>Kamlesh Nagarkoti</h2>
+      <h2>Mitchell Marsh</h2>
+      <h2>Ripal Patel</h2>
+      <h2>Vicky Ostwal</h2>
+      <h2>Aman Khan</h2>
+      <h2>Chetan Sakariya</h2>
+      <h2>Lungi Ngidi</h2>
+      <h2>Khaleel Ahmed</h2>
+    
+   
+    <div className="rcb">
+      <h1>Royal Challengers Bangalore squad for IPL 2023</h1>
+      <h2>Virat Kohli. Captain</h2>
+      <h2>Rajat Patidar</h2>
+      <h2>Anuj Rawat</h2>
+      <h2>David Willey</h2>
+      <h2>Harshal Patel</h2>
+      <h2>Shahbaz Ahamad</h2>
+      <h2>Wanindu Hasaranga</h2>
+      <h2>Josh Hazlewood</h2>
+      <h2>Siddharth Kaul</h2>
+      <h2>Reece Topley</h2>
+
+
+    <div className="kkr">
+      <h1>Kolkata Knight Riders squad for IPL 2023</h1>
+      <h2>Nitish Rana. Captain</h2>
+      <h2>Rinku Singh</h2>
+      <h2>David Wiese</h2>
+      <h2>Kulwant Khejroliya</h2>
+      <h2>Lockie Ferguson</h2>
+      <h2>Venkatesh Iyer</h2>
+      <h2>Harshit RanaSuyash Sharma</h2>
+      <h2>Shreyas Iyer</h2>
+      <h2>Umesh Yadav</h2>
+      <h2>Sunil Narine</h2>
+    
+
+    <div className="csk">
+      <h1>Chennai Super Kings squad for IPL 2023</h1>
+      <h2>MS Dhoni. Captain</h2>
+      <h2>Ruturaj Gaikwad</h2>
+      <h2>Ambati Rayudu</h2>
+      <h2>Dwaine Pretorius</h2>
+      <h2>Bhagath Varma</h2>
+      <h2>Deepak Chahar</h2>
+      <h2>Devon Conway</h2>
+      <h2>Ravindra Jadeja</h2>
+      <h2>Shivam Dube</h2>
+      <h2>Simarjeet Singh</h2>
+      
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+   
+  );
+}
+
+export default App;
